9239ddd09fb7bf1729dfed81543fdf4528dcc2c2
Alexandre Lavoie <tatti6204@hotmail.com>
2019-11-03 01:13:52 -0700

    Small changes to before deleting files.

--- Assets/Scripts/MHArea.cs
+++ Assets/Scripts/MHArea.cs
@@ -1,36 +1,66 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using MLAgents;
 
 public class MHArea : Area
 {
     public MHAgent mhAgent;
+    public GameObject anchor;
     public List<GameObject> masses;
+    public GameObject massPrefab;
     public int numberOfMasses;
     public int stringLength;
     public string logString;
 
     public override void ResetArea()
     {
-
+        ResetMasses();
     }
 
-    void resetMasses()
+    void ResetMasses()
     {
-        if(masses.Count == numberOfMasses){
-            foreach(GameObject mass in masses){
-
+        if (masses.Count == numberOfMasses)
+        {
+            for (int i = 0; i < masses.Count; i++)
+            {
+                if (i == 0)
+                {
+                    masses[i].transform.position = RandomSphericalCoordinate(anchor.transform.position);
+                }
+                else
+                {
+                    masses[i].transform.position = RandomSphericalCoordinate(masses[i - 1].transform.position);
+                }
+                masses[i].transform.rotation = Quaternion.Euler(0, 0, 0);
+                masses[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
             }
+        }
+        else
+        {
+            InitializeMasses();
         }
     }
 
-    Vector3 randomSphericalCoordinate(Vector3 origin){
-        float phi = Random.Range(0f, Mathf.PI / 2f);
-        float theta = Random.Range(0f, 2f * Mathf.PI);
-        return new Vector3(Mathf.Cos(theta), 0, Mathf.Sin(theta));
+    void InitializeMasses()
+    {
+        masses = new List<GameObject>();
+
+        for(int i = 0; i < numberOfMasses; i++){
+            masses.Add(Instantiate(massPrefab, new Vector3(transform.position.x, -5f * i, transform.position.z), Quaternion.Euler(0, 0, 0)));
+        }
+
+        ResetMasses();
     }
-    public bool IsStable() {
+
+    Vector3 RandomSphericalCoordinate(Vector3 origin)
+    {
+        float phi = Random.Range(0f, Mathf.PI);
+        float theta = Random.Range(0f, 2f * Mathf.PI);
+        return stringLength * new Vector3(Mathf.Cos(theta) * Mathf.Sin(phi), Mathf.Cos(phi), Mathf.Cos(phi) * Mathf.Sin(theta));
+    }
+
+    public bool IsStable()
+    {
         return false;
     }
 }
