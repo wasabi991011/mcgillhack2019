be971349062eb9fcf49c33eb076b1546790f61ee
Alexandre Lavoie <tatti6204@hotmail.com>
2019-11-03 00:35:43 -0700

    Template file for area.

--- Assets/Scripts/MHArea.cs
+++ Assets/Scripts/MHArea.cs
@@ -2,89 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using MLAgents;
-using MathNet.Numerics.Distributions;
 
 public class MHArea : Area
 {
     public MHAgent mhAgent;
-    public List<GameObject> cubes;
-    public GameObject cubePrefab;
-    public int numberOfCubes = 2;
-    public float beta = 0;
-    public string logString = "";
+    public List<GameObject> masses;
+    public int numberOfMasses;
+    public int stringLength;
+    public string logString;
 
     public override void ResetArea()
     {
-        resetCubes();
-        //Debug.Log(logString);
-        logString = "";
+
     }
 
-    float randomMass(bool high)
+    void resetMasses()
     {
-        Beta betaDist;
-        if (high)
-        {
-            betaDist = new Beta(beta, 2);
-        }
-        else
-        {
-            betaDist = new Beta(2, beta);
-        }
-        return (9f * (float)betaDist.Sample()) + 1f;
-    }
+        if(masses.Count == numberOfMasses){
+            foreach(GameObject mass in masses){
 
-    void resetCubes()
-    {
-        if(cubes.Count == numberOfCubes)
-        {
-            int highIndex = Random.Range(0, numberOfCubes);
-            for (int i = 0; i < numberOfCubes; i++)
-            {
-                cubes[i].transform.position = new Vector3(this.transform.position.x, i * 5.0f + 1f, this.transform.position.z);
-                cubes[i].transform.rotation = Quaternion.Euler(Vector3.zero);
-                cubes[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
-                cubes[i].GetComponent<Rigidbody>().mass = randomMass(i==highIndex);
             }
-        } else
-        {
-            createCubes();
         }
     }
 
-    void createCubes()
-    {
-        int highIndex = Random.Range(0, numberOfCubes);
-        for (int i = 0; i < numberOfCubes; i++)
-        {
-            GameObject temporaryCube = Instantiate(cubePrefab, new Vector3(this.transform.position.x, i * 5.0f + 1f, this.transform.position.z), Quaternion.Euler(Vector3.zero));
-
-            temporaryCube.GetComponent<Rigidbody>().mass = randomMass(i==highIndex);
-
-            temporaryCube.transform.parent = this.transform;
-
-            cubes.Add(temporaryCube);
-        }
+    Vector3 randomSphericalCoordinate(Vector3 origin){
+        float phi = Random.Range(0f, Mathf.PI / 2f);
+        float theta = Random.Range(0f, 2f * Mathf.PI);
+        return new Vector3(Mathf.Cos(theta), 0, Mathf.Sin(theta));
     }
-
-    public Dictionary<int, int> getAnswerKey()
-    {
-        List<int> indices = new List<int>();
-
-        for (int i=0; i<numberOfCubes; i++)
-        {
-            indices.Add(i);
-        }
-
-        indices.Sort((x, y) => ((cubes[y].GetComponent<Rigidbody>().mass > cubes[x].GetComponent<Rigidbody>().mass) ? 1 : -1));
-
-        Dictionary<int, int> reversedIndices = new Dictionary<int, int>();
-
-        for (int i = 0; i < numberOfCubes; i++)
-        {
-            reversedIndices.Add(indices[i], i);
-        }
-        // Returns a Dictionary where you give it the object index as a key and returns mass ranking.
-        return reversedIndices;
+    public bool IsStable() {
+        return false;
     }
 }
